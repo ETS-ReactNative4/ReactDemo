32286527cf642a67b0bf16066a9f6135b3cb145c
Medal Xing <daojiyan@gmail.com>
2019-04-10 16:17:27 -0400

    components split

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./Cockpit.css";
+
+const cockpit = (props) =>{
+    let assignedclasses = [];
+    let btnClass = "";
+    if (props.showPersons){
+        btnClass = classes.Red;
+    }
+    
+    if (props.persons.length <=2){
+      assignedclasses.push(classes.red);
+    }
+    if (props.persons.length <=1){
+      assignedclasses.push (classes.bold);
+    }
+    return(
+        <div className={classes.Cockpit}>
+            <h1>{props.title}</h1>
+            <p className={assignedclasses.join(" ")}>This is really working!</p>
+            <button className = {btnClass} onClick={props.clicked}>Show/Hide</button>
+        </div>
+    );
+}
+
+export default cockpit;
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Person.css";
+import classes from "./Person.css";
 // import Radium from 'radium';
 
 const person = ( props ) => {
@@ -8,8 +8,13 @@ const person = ( props ) => {
     //         width: "450px"
     //     }
     // }
+    // const rnd = Math.random();
+
+    // if (rnd > 0.7){
+    //     throw new Error("WRONG WRONG WRONG!");
+    // }
     return (
-        <div className="Person" style={style}>
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
